3611d249378866d42aaa4e8325cac3cfa234aa29
parth chotaliya <42476875+p-chotaliya@users.noreply.github.com>
2019-06-22 14:33:52 +0530

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!--
-██████╗ ███████╗███████╗██████╗ ██████╗ 
-██╔══██╗██╔════╝██╔════╝██╔══██╗╚════██╗
-██║  ██║█████╗  █████╗  ██████╔╝ █████╔╝
-██║  ██║██╔══╝  ██╔══╝  ██╔═══╝  ╚═══██╗
-██████╔╝███████╗███████╗██║     ██████╔╝
-╚═════╝ ╚══════╝╚══════╝╚═╝     ╚═════╝ 
-         By Manisso & IcoDz  
--->
+
 <html class="js not-logged-in client-root" lang="en"><!--<![endif]--><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
